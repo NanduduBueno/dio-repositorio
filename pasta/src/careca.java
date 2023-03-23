693fe86d6883d64959d830741d76fc2cfa00952b
NanduduBueno <eduardo.eodab@gmail.com>
2023-03-23 10:27:40 -0300

    commmmiiiittttt

--- pasta/src/careca.java
+++ pasta/src/careca.java
@@ -2,5 +2,6 @@ public class careca {
     public static void main(String[] args) throws Exception {
         System.out.println("World!");
         System.out.println("repositorio");
+        System.out.println("reste");
     }
 }

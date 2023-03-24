7c3c191e7a1c4880a1dcbd499734a1c9107b364d
NanduduBueno <eduardo.eodab@gmail.com>
2023-03-23 21:44:55 -0300

    commit?

--- pasta/src/careca.java
+++ pasta/src/careca.java
@@ -1,4 +1,4 @@
-public class careca {
+public class campo1 {
     public static void main(String[] args) throws Exception {
         System.out.println("World!");
         System.out.println("repositorio");
